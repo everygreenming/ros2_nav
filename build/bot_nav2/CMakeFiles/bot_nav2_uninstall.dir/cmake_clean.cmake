b9016046649bb823b018241585f0657dd4c1e72e
waitblue <2245281871@qq.com>
2026-05-24 19:57:05 +0800

    fix

--- build/bot_nav2/CMakeFiles/bot_nav2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/bot_nav2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bot_nav2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/waypoint/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/everygreen/ros2/bot_ws/install/waypoint'
