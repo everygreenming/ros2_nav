3154e1e4e519f825fd5c380670dfd45876d0528c
everygreenming <everygreenming@users.noreply.github.com>
2026-05-26 17:30:50 +0800

    feat: update initial pose coordinates in init_bot_pose to match spawn point

--- src/waypoint/waypoint/init_bot_pose.py
+++ src/waypoint/waypoint/init_bot_pose.py
@@ -9,9 +9,10 @@ def main():
     initial_pose = PoseStamped()
     initial_pose.header.frame_id = 'map'
     initial_pose.header.stamp = navigator.get_clock().now().to_msg()
-    initial_pose.pose.position.x = 0.0
-    initial_pose.pose.position.y = 0.0
-    initial_pose.pose.orientation.w = 1.0
+    initial_pose.pose.position.x = 0.3814
+    initial_pose.pose.position.y = 3.4835
+    initial_pose.pose.orientation.z = 0.04998
+    initial_pose.pose.orientation.w = 0.99875
     navigator.setInitialPose(initial_pose)
     navigator.waitUntilNav2Active()
     rclpy.spin(navigator)
